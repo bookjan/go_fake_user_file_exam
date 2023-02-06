5979d38e2483c40406b7320888707cc53d6eeda8
Johnson Jan Su <su1221jan@gmail.com>
2023-02-06 13:25:08 +0800

    Update run.sh to align the test case

--- run.sh
+++ run.sh
@@ -2,18 +2,15 @@
 
 ./run.out - << EOF
 register user1
-create_folder user1 'Work' 'The working files and necessary files are here'
-create_folder user1 'Fun' 'The fun files'
-create_folder user1 'Apple' 'The apple files'
-create_folder user1 'Book' 'The book files'
-create_folder user1 'Car' 'The Car files'
+create_folder user1 ‘Work’ ‘The working files and necessary files are here’
 get_folders user1
-get_folders user1 sort_name desc
-get_folders user1 sort_time asc
-get_folders user1 sort_time desc
-rename_folders user1 1001 Jello
-get_folders user1
-upload_file user1 1002 '1.tc' 'first test case for a company'
-delete_file user1 1002 1.tc
+rename_folder user1 1001 ‘Temp’
+delete_folder user1 1001
+create_folder user1 ‘Testing’ ‘The testing folders’
+upload_file user1 1002 ‘1.tc’ ‘first test case for a company’
+upload_file user1 1002 ‘1.png’ ‘the picture for first test case’
+get_files user1 1002 sort_extension asc
+delete_file user1 1002 1.png
+delete_file user1 1001 1.tc
 exit
 EOF
